c57b0c830f0db11ac2c83beefeb04a69d381c2d3
ramanan <nnraam@hotmail.com>
2020-11-06 22:55:40 +0100

    COMMIT BOODSCHAP

---
+++ script.js
@@ -0,0 +1,16 @@
+const huiswerkMaken = function(vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+
+    callback();
+};
+const klaarMetHuiswerk = function() {
+    console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
+};
+huiswerkMaken("wiskunde", klaarMetHuiswerk);
+
+const huiswerkMaken = function(vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    setTimeout(function() {
+        callback();
+    }, 10000);
+};
